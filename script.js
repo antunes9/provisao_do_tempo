5432be8ecd79d6fc0b87686b5f17bd9be6760541
Antunes Tomás Moco Bento <antunes.tomas@unipiaget-angola.org>
2024-06-19 10:45:18 +0100

    primeiro comit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+const key = "391af9f2acdd7532e843ae803fe1063f"
+
+function colocarDadosNaTela(dados){
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em: " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + " ºC"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+async function buscarCidade(cidade){
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
+
+    colocarDadosNaTela(dados)
+  
+
+}
+
+function cliqueiNoBotao() {
+
+    const cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+}
